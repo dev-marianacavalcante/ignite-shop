6030d68b1f3f12479516d22326cfc14fbbd7619d
dev-marianacavalcante <mariana.cavalcantevl@gmail.com>
2023-08-07 15:54:42 -0300

    correcao

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,11 +26,15 @@ export default function Home({ products }: HomeProps) {
       spacing: 48,
     },
   });
+
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map((product) => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
+          <Product
+            href={`/product/${product.id}`}
+            key={product.id}
+            className="keen-slider__slide">
             <Image src={product.imageUrl} alt="" width={520} height={480} />
 
             <footer>
@@ -56,7 +60,10 @@ export const getStaticProps: GetStaticProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount / 100,
+      price: new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(price.unit_amount / 100),
     };
   });
 
@@ -64,6 +71,6 @@ export const getStaticProps: GetStaticProps = async () => {
     props: {
       products,
     },
-    revalidate: 60 * 60 * 2, // 2 hours
+    // revalidate: 60 * 60 * 2, // 2 hours
   };
 };

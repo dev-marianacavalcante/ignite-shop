df56896c4296ad7e9883b687846b636f9fdcedcd
dev-marianacavalcante <mariana.cavalcantevl@gmail.com>
2023-08-07 18:44:49 -0300

    Estrutura da pagina de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,27 @@
 import { useRouter } from "next/router";
 import React from "react";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "../../styles/pages/product";
 
 export default function product() {
   const { query } = useRouter();
-  return <h1>product {JSON.stringify(query)}</h1>;
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+          asperiores placeat sint labore similique, voluptate consequuntur
+          quidem facere molestias tenetur eos, quam, aspernatur culpa fuga!
+          Debitis quo enim ipsam nobis!
+        </p>
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }

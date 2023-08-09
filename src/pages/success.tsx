41577647ff7df0d4f940e3996f1f8098c60ca510
dev-marianacavalcante <mariana.cavalcantevl@gmail.com>
2023-08-09 09:07:26 -0300

    Estrutura da pagina de success

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { SucessContainer, ImageContainer } from "../styles/pages/success";
+import Link from "next/link";
 
 export default function success() {
-  return <h1>success</h1>;
+  return (
+    <SucessContainer>
+      <h1>Compra efetuada!</h1>
+      <ImageContainer></ImageContainer>
+      <p>
+        Uhuul <strong>Diego Fernandes</strong>, sua{" "}
+        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
+        casa.
+      </p>
+      <Link href="/">Voltar ao catalogo</Link>
+    </SucessContainer>
+  );
 }
